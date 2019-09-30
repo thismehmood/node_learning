45a3c72263dc0a86fc1624ac4487162c0fbc5865
Mehmood <this.mehmood@gmail.com>
2019-09-30 12:05:46 -0700

    spacing changing

--- printing_in_colors/npm_colors.js
+++ printing_in_colors/npm_colors.js
@@ -24,9 +24,10 @@ console.log(chalk.hex('#DEADED').bold('Bold gray!'));
 const chalk = require('chalk');
  
 const miles = 18;
+
 const calculateFeet = miles => miles * 5280;
  
 console.log(chalk`
   There are {bold 5280 feet} in a mile.
-  In {bold ${miles} miles}, there are {green.bold ${calculateFeet(miles)} feet}.
+  In {bold ${ miles } miles}, there are {green.bold ${calculateFeet(miles)} feet}.
 `);

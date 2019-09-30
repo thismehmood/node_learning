a09a1761a2f822bd7f2460eaf1348ee0ba97b535
Mehmood <this.mehmood@gmail.com>
2019-09-30 10:43:50 -0700

    npm colors add

---
+++ printing_in_colors/npm_colors.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+var Mmsg =  chalk.yellow.bold.inverse('we all faced imposible things ever'); 
+console.log(chalk.red('We are all faced with a series of great opportunities brilliantly disguised as impossible situations', chalk.underline.bgBlue('disguised') + '!'));
+console.log(Mmsg);
+

e8052d2a29064cc877857e7db0c4f804dde65e55
Mehmood <this.mehmood@gmail.com>
2019-10-01 23:33:12 +0500

    input taken

--- taking_input_from_user.js
+++ taking_input_from_user.js
@@ -1,6 +1,8 @@
-const command = process.argv[3];
+const command = process.argv[2];
 //console.log(command);
 if (command === 'add'){
     console.log('adding something happen');
 
+} else if (command === 'removed'){
+    console.log('removing done')
 }

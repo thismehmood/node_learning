63ce98d64f452f3fb775b205bc72d2f23a6016c7
Mehmood <this.mehmood@gmail.com>
2019-10-01 23:56:47 +0500

    case slice in JS

--- taking_input_from_user.js
+++ taking_input_from_user.js
@@ -11,4 +11,17 @@ if (command === 'add'){
 var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 var citrus = fruits.slice(1, 3);
 console.log(citrus)
+//=============================================
+var myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
 
+switch (myArgs[0]) {
+case 'insult':
+    console.log(myArgs[1], 'smells quite badly.');
+    break;
+case 'compliment':
+    console.log(myArgs[1], 'is really cool.');
+    break;
+default:
+    console.log('Sorry, that is not something I know how to do.');
+}

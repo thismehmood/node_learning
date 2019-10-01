279db12f0ada1d2b012a605fd731ef0e3e9fdd10
Mehmood <this.mehmood@gmail.com>
2019-10-01 23:54:14 +0500

    slice happens in JS

--- taking_input_from_user.js
+++ taking_input_from_user.js
@@ -6,3 +6,9 @@ if (command === 'add'){
 } else if (command === 'removed'){
     console.log('removing done')
 }
+//The slice() method returns the selected elements in an array, as a new array object.
+//The slice() method selects the elements starting at the given start argument, and ends at, but does not include, the given end argument.
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1, 3);
+console.log(citrus)
+

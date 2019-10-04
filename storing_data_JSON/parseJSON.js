176616f6f35eb0960c8c56d72ba09999bf61c266
Mehmood <this.mehmood@gmail.com>
2019-10-04 23:52:34 +0500

    parsestrigify add

---
+++ storing_data_JSON/parseJSON.js
@@ -0,0 +1,10 @@
+//The JSON.parse() method parses a JSON string, 
+//constructing the JavaScript value or object described by the string
+var json = '{"count":24 , "integers":2334577}';
+obj = JSON.parse(json);
+console.log(obj);
+console.log(JSON.parse('{"p": 5}', (key, value) =>
+  typeof value === 'number'
+    ? value * 2 // return value * 2 for numbers
+    : value     // return everything else unchanged
+));

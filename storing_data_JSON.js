b5eb679aecc2da319c0befb031644594f0aed2b9
Mehmood <this.mehmood@gmail.com>
2019-10-04 22:57:09 +0500

    JSON_data_store

---
+++ storing_data_JSON.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const  book = {
+    title: ' somethings happens for goods',
+    author: 'kiyonsaki'
+    }
+    const bookJSON = JSON.stringify(book);
+    console.log(bookJSON);
+

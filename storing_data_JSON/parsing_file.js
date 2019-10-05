a2aa67255dffb294a252a6c8e102f210c304a061
Mehmood <this.mehmood@gmail.com>
2019-10-05 23:41:17 +0500

    readfile_using_JSON

--- storing_data_JSON/parsing_file.js
+++ storing_data_JSON/parsing_file.js
@@ -3,5 +3,10 @@ const book = {
     title:'egg is benificial for health',
     auther:'robert '
 }
-const bookjson = JSON.stringify(book);
-fs.writeFileSync('write.JSON',bookjson);
+
+ const bookjson = JSON.stringify(book);
+ fs.writeFileSync('write.JSON',bookjson);
+const buffer = fs.readFileSync('write.JSON');
+const databuffer = buffer.toString();
+const data = JSON.parse(databuffer);
+console.log(data.title;

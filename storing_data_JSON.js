8fc13fd57a8268269d6b8f12b6baf1bfb4f0693a
Mehmood <this.mehmood@gmail.com>
2019-10-15 23:59:44 +0500

    something changes

--- storing_data_JSON.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const  book = {
-    title: ' somethings happens for goods',
-    author: 'kiyonsaki'
-    }
-    const bookJSON = JSON.stringify(book);
-    console.log(bookJSON);
-

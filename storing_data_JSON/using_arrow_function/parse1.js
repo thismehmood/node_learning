781607082b6d337130184933636b3da5456b00e9
Mehmood <this.mehmood@gmail.com>
2019-10-29 23:56:15 +0500

    arrow_function

---
+++ storing_data_JSON/using_arrow_function/parse1.js
@@ -0,0 +1,9 @@
+    const square = function(x){
+        return x*x;
+    }
+    console.log(square(3))
+    const squar = (x) =>{
+           return x*x;
+    }
+    const suqae = (x) => x*x;
+    console.log(suqae(5));
